484af9f1844110f7f8987b0bb0bdd579c6308399
pt20krezolekk <103023548+pt20krezolekk@users.noreply.github.com>
2024-05-28 11:10:06 +0200

    1.20.6

---
+++ NeoForge/src/main/java/me/khajiitos/sussyendereyes/neoforge/SussyEnderEyesNeoForge.java
@@ -0,0 +1,11 @@
+package me.khajiitos.sussyendereyes.neoforge;
+
+import me.khajiitos.sussyendereyes.common.SussyEnderEyes;
+import net.neoforged.fml.common.Mod;
+
+@Mod(SussyEnderEyes.MOD_ID)
+public class SussyEnderEyesNeoForge {
+    public SussyEnderEyesNeoForge() {
+
+    }
+}
